9f2d09726b69511fcc09ea1299b846e2d3938bac
NicoClemente <clementes.nicolas@gmail.com>
2022-11-27 23:57:18 -0300

    commit & push

---
+++ js/ejecutar.js
@@ -0,0 +1,14 @@
+function inclusive() {
+    document.body.style.backgroundColor = "black"
+    document.body.style.color = "white"
+    document.getElementById("header").style.backgroundColor = "grey"
+
+   
+   
+  }
+  
+  function copy() {
+    alert("Autor: Nicolás Clemente. EMAIL: clementes.nicolas@gmail.com")
+  }
+  
+  
