aa40c6207675d4f6e7c1d7d1ebd847e731848d50
NicoClemente <clementes.nicolas@gmail.com>
2022-12-07 02:01:24 -0300

    dark mode

---
+++ js/main.js
@@ -0,0 +1,24 @@
+
+alert("チンチョンチンチョン")
+let count=0;
+function inclusive(){
+    count++
+     
+
+if (count==1){ $(".theme").attr("href", "./style/dark.css");
+
+document.getElementById("lunita").style.color="red"
+}
+    else{
+        $(".theme").attr("href", "./style/style.css");
+        count=0;
+        document.getElementById("lunita").style.color="beige";
+    }
+
+    }
+
+function copy(){
+    alert("Nombre: Nicolás Clemente. Mail:clementes.nicolas@gmail.com")
+}
+    
+
